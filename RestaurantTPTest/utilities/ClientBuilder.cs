f0997e2894de0638f5f01a64f0f823f86c72d11f
theo <theo.simoes@hotmail.fr>
2021-12-22 12:16:33 +0100

    re-add some tests

---
+++ RestaurantTPTest/utilities/ClientBuilder.cs
@@ -0,0 +1,18 @@
+namespace LeGrandRestaurant.Test.Utilities
+{
+  internal class ClientBuilder
+  {
+    private string _nom = string.Empty;
+
+    public ClientBuilder Nommé(string nom)
+    {
+      _nom = nom;
+      return this;
+    }
+
+    public Client Build()
+    {
+      return new Client(_nom);
+    }
+  }
+}
