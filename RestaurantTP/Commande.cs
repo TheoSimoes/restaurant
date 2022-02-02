9220034f02837d460368b3dc9d4d9539fbacf45a
Vivien-T <thiebaut.vivien@gmail.com>
2022-02-02 14:29:43 +0100

    produit + commande

---
+++ RestaurantTP/Commande.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantTP
+{
+    class Commande
+    {
+        Serveur _serveur;
+        Client _client;
+        List<Produit> _produits;
+    }
+}
