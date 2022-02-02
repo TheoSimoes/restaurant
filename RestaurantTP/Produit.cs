0d51765ca1c837252e584487705045a5c9edd73e
ljoub <45665062+ljoub@users.noreply.github.com>
2022-02-02 14:49:10 +0100

    produit

--- RestaurantTP/Produit.cs
+++ RestaurantTP/Produit.cs
@@ -6,5 +6,19 @@ namespace RestaurantTP
 {
     class Produit
     {
+
+        public float Prix { get; private set; }
+        public string Nom { get; private set; }
+        public int Stock { get; private set; }
+        public string Statut { get; set; }
+    
+        public Produit(int prix, string nom, int stock, string statut)
+        {
+            Prix = prix;
+            Nom = nom;
+            Stock = stock;
+            Statut = statut;
+        }
+    
     }
 }
